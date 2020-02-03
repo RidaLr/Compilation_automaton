db3b2a0a59cfcf90d89eb0dbe7a419e8d492c931
RidaLr <32525321+RidaLr@users.noreply.github.com>
2020-02-03 14:01:26 +0100

    Update structures.h

--- structures.h
+++ structures.h
@@ -44,14 +44,4 @@ typedef struct AFD{
 }AFD;
 
 
-AFND automate_vide();
-AFND automate_Seul_Mot_vide();
-AFND automate_standard(char c);
-
-AFND reunion_automate(AFND a1, AFND a2);
-AFND concatenation_automate(AFND a1, AFND a2);
-AFND farmeture_automate(AFND automate);
-void Display(AFND automate);
-
-
 #endif
