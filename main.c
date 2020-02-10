5de71e5ce1e733b1f8f106f4094e9c665ec5b292
RidaLr <32525321+RidaLr@users.noreply.github.com>
2020-02-10 22:47:00 +0100

    Add UNION test & CONCATENATION test

--- main.c
+++ main.c
@@ -12,5 +12,24 @@ int main()
 	AFND *automate2 = automate_vide();
 	Display(automate2);
 	
+	AFND *automate3 = automateTestPlein();
+	AFND *automate5 = automateTestPlein();
+	
+	Display(automate5);
+
+	
+	AFND *automate4 = reunion_automate(automate3, automate5);
+	printf("\n UNION \n");
+	Display(automate4);
+	
+	AFND *automate8 = automateTestPlein();
+	AFND *automate9 = automateTestPlein();
+	
+	AFND *automate6 = concatenation_automate(automate8, automate9);
+	printf("\n CONCATENATION \n");
+	Display(automate6);
+	
+	//AFND *automate7 = farmeture_automate(automate3);
+	
 	return 0;
 }
