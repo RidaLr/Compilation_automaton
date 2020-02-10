d765ca7eec2ca4e55af50cb8aee5c51147995e2d
RidaLr <32525321+RidaLr@users.noreply.github.com>
2020-02-10 22:48:44 +0100

    Update non_deterministic.h

--- non_deterministic.h
+++ non_deterministic.h
@@ -5,10 +5,11 @@
 #include <stdlib.h>
 AFND *automate_vide();
 AFND *automate_Seul_Mot_vide();
-AFND automate_standard(char c);
+AFND *automate_standard(char c);
 
-AFND reunion_automate(AFND a1, AFND a2);
-AFND concatenation_automate(AFND a1, AFND a2);
-AFND farmeture_automate(AFND automate);
+AFND *reunion_automate(AFND *a1, AFND *a2);
+AFND *concatenation_automate(AFND *a1, AFND *a2);
+AFND *farmeture_automate(AFND *automate);
 void Display(AFND *automate);
+AFND *automateTestPlein();
 #endif
