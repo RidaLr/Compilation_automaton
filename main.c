911da8fd6203a7e6eb3709e023ac7d5b45c6f101
RidaLr <32525321+RidaLr@users.noreply.github.com>
2020-02-12 21:42:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,35 +6,9 @@
 
 int main()
 {
-	AFND *automate = automate_Seul_Mot_vide();
-	Display(automate);
-
-	AFND *automate2 = automate_vide();
-	Display(automate2);
-	
-	AFND *automate3 = automateTestPlein();
-	AFND *automate5 = automateTestPlein();
-	
-	Display(automate5);
-
-	
-	AFND *automate4 = reunion_automate(automate3, automate5);
-	printf("\n UNION \n");
-	Display(automate4);
-	
-	AFND *automate8 = automateTestPlein();
-	AFND *automate9 = automateTestPlein();
-	
-	AFND *automate6 = concatenation_automate(automate8, automate9);
-	printf("\n CONCATENATION \n");
-	Display(automate6);
 	
 	
-	AFND *aFermTest = automateTestPlein();
-	
-	AFND *automate7 = farmeture_automate(aFermTest);
-	printf("\n FERMETURE \n");
-	Display(automate7);
+	Test_AFND();
 	
 	return 0;
 }
